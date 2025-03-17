7c2011f9052cc1379a4e7b9cb207f283a0a41241
Queen Accila <helenho2k1@gmail.com>
2025-03-16 20:34:11 -0700

    Working on basic columns for either interaction

--- acci-landing-page/src/App.jsx
+++ acci-landing-page/src/App.jsx
@@ -8,26 +8,29 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Acci's Portfolio</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="column-container">
+        {/* Interactive mode */}
+        <div class="column-item">
+          <h2>Interactive Mode</h2>
+          <a href="https://game.accila.com/" target="_blank">
+            <img src={viteLogo} className="column-image"/>
+          </a>
+          <p>
+            Click here for a more interactable experience for my portfolio!
+          </p>
+        </div>
+
+        {/* Standard mode */}
+        <div class="column-item">
+          <h3>Standard Mode</h3>
+        </div>
+      </div>
     </>
   )
 }
